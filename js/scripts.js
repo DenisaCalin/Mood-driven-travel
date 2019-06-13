e73f0c40926042daa98375fe3a852abf22c005f7
Denisa Calin <denisa.calin@teamextension.ro>
2019-06-13 23:02:17 +0300

    hero

---
+++ js/scripts.js
@@ -0,0 +1,66 @@
+const h = document.querySelector(".sticky-part");
+let stuck = false;
+let stickPoint = getDistance(h);
+
+function getDistance(h) {
+
+    if (h) {
+        let topDist = h.offsetTop;
+        return topDist;
+    }
+}
+
+window.onscroll = function(e) {
+    let distance = getDistance(h) - window.pageYOffset;
+    let offset = window.pageYOffset;
+
+    if ( (distance <= 0) && !stuck) {
+        document.body.classList.add('header-sticky');
+        stuck = true;
+    } else if (stuck && (offset <= stickPoint)){
+        document.body.classList.remove('header-sticky');
+        stuck = false;
+    }
+}
+
+const nav = document.querySelector("nav");
+const burgerNav = document.querySelector(".burger-nav");
+const closeNav = document.querySelector("nav .close-nav");
+
+if (burgerNav) {
+  burgerNav.onclick = () => {
+    nav.classList.add("slide-out");
+    dropdownHolder.classList.remove("dropdown-active");
+  }
+}
+
+if (closeNav) {
+  closeNav.onclick = () => {
+    nav.classList.remove("slide-out");
+    dropdownHolder.classList.remove("dropdown-active");
+  }
+}
+
+const dropdownHolder = document.querySelector(".dropdown-holder");
+if (dropdownHolder) {
+  const link = dropdownHolder.querySelector("a");
+
+  if (link) {
+    link.onclick = () => {
+      if (window.innerWidth <= 992) {
+        dropdownHolder.classList.toggle("dropdown-active");
+      }
+    }
+
+    link.addEventListener("click", function(event) {
+      if (window.innerWidth <= 992) {
+        event.preventDefault();
+      }
+    });
+  }
+}
+
+$('.carousel').carousel({
+  interval: 6000,
+  pause: "false"
+});
