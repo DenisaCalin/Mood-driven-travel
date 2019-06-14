cea9eacf25f6ab2202e89bf2121e94d3af8828df
Denisa Calin <denisa.calin@teamextension.ro>
2019-06-14 20:25:26 +0300

    smth

--- js/scripts.js
+++ js/scripts.js
@@ -60,7 +60,18 @@ if (dropdownHolder) {
   }
 }
 
-$('.carousel').carousel({
-  interval: 6000,
-  pause: "false"
+$('.carousel.carousel-multi-item.v-2 .carousel-item').each(function(){
+  var next = $(this).next();
+  if (!next.length) {
+    next = $(this).siblings(':first');
+  }
+  next.children(':first-child').clone().appendTo($(this));
+
+  for (var i=0;i<4;i++) {
+    next=next.next();
+    if (!next.length) {
+      next=$(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+  }
 });
